70c6eb3e13d1147e787c67d28dd2f5ce7742b93b
Marta G <marta.guszek@gmail.com>
2020-04-08 15:06:40 +0200

    initial commit

---
+++ createModal.js
@@ -0,0 +1,45 @@
+function createModal() {
+  //create the main body of the popup;
+  const body = document.getElementById('bd');
+
+  const mainDiv = document.createElement('div');
+  mainDiv.classList.add('modal');
+  mainDiv.id = 'popup';
+
+  body.appendChild(mainDiv);
+
+  const modalContent = document.createElement('div');
+  modalContent.classList.add('modal-content');
+  mainDiv.appendChild(modalContent);
+
+  const h2 = document.createElement('h2');
+  h2.classList.add('headline');
+  h2.textContent = 'GDPR consent';
+  modalContent.appendChild(h2);
+
+  const legalText = document.createElement('div');
+  legalText.id = 'legal';
+  modalContent.appendChild(legalText);
+
+  const list = document.createElement('div');
+  list.id = 'listOfVendors';
+  modalContent.appendChild(list);
+
+  const btns = document.createElement('div');
+  btns.classList.add('buttons');
+  modalContent.appendChild(btns);
+
+  const acceptButton = document.createElement('button');
+  const rejectButton = document.createElement('button');
+
+  acceptButton.id = 'accept';
+  rejectButton.id = 'reject';
+
+  acceptButton.textContent = 'Accept';
+  rejectButton.textContent = 'Reject';
+
+  btns.appendChild(acceptButton);
+  btns.appendChild(rejectButton);
+}
+
+createModal()
