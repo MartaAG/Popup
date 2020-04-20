c9b13616f0343083a21924b560f09637676e8356
Marta G <marta.guszek@gmail.com>
2020-04-20 13:46:28 +0200

    add first working test for create modal function

--- createModal.js
+++ createModal.js
@@ -1,45 +1,49 @@
-function createModal() {
+function createModal() {}
   //create the main body of the popup;
-  const body = document.getElementById('bd');
+createModal.prototype.init = function() {
+    const body = document.getElementsByTagName('BODY')[0];
 
-  const mainDiv = document.createElement('div');
-  mainDiv.classList.add('modal');
-  mainDiv.id = 'popup';
+    const mainDiv = document.createElement('div');
+    mainDiv.classList.add('modal');
+    mainDiv.id = 'popup';
 
-  body.appendChild(mainDiv);
+    body.appendChild(mainDiv);
 
-  const modalContent = document.createElement('div');
-  modalContent.classList.add('modal-content');
-  mainDiv.appendChild(modalContent);
+    const modalContent = document.createElement('div');
+    modalContent.classList.add('modal-content');
+    mainDiv.appendChild(modalContent);
 
-  const h2 = document.createElement('h2');
-  h2.classList.add('headline');
-  h2.textContent = 'GDPR consent';
-  modalContent.appendChild(h2);
+    const h2 = document.createElement('h2');
+    h2.classList.add('headline');
+    h2.textContent = 'GDPR consent';
+    modalContent.appendChild(h2);
 
-  const legalText = document.createElement('div');
-  legalText.id = 'legal';
-  modalContent.appendChild(legalText);
+    const legalText = document.createElement('div');
+    legalText.id = 'legal';
+    modalContent.appendChild(legalText);
 
-  const list = document.createElement('div');
-  list.id = 'listOfVendors';
-  modalContent.appendChild(list);
+    const list = document.createElement('div');
+    list.id = 'listOfVendors';
+    modalContent.appendChild(list);
 
-  const btns = document.createElement('div');
-  btns.classList.add('buttons');
-  modalContent.appendChild(btns);
+    const btns = document.createElement('div');
+    btns.classList.add('buttons');
+    modalContent.appendChild(btns);
 
-  const acceptButton = document.createElement('button');
-  const rejectButton = document.createElement('button');
+    const acceptButton = document.createElement('button');
+    const rejectButton = document.createElement('button');
 
-  acceptButton.id = 'accept';
-  rejectButton.id = 'reject';
+    acceptButton.id = 'accept';
+    rejectButton.id = 'reject';
 
-  acceptButton.textContent = 'Accept';
-  rejectButton.textContent = 'Reject';
+    acceptButton.textContent = 'Accept';
+    rejectButton.textContent = 'Reject';
 
-  btns.appendChild(acceptButton);
-  btns.appendChild(rejectButton);
+    btns.appendChild(acceptButton);
+    btns.appendChild(rejectButton);
+
+    return {
+      mainDiv, modalContent, h2, legalText, list, btns,
+      acceptButton, rejectButton
+    }
 }
-
-createModal()
---
+++ spec/specCreateModal.js
@@ -0,0 +1,70 @@
+
+
+describe('Modal', function() {
+  let Dom;
+beforeEach(() => {
+  Dom = new createModal();
+})
+
+
+it ('should initialize', () => {
+  const body = document.getElementsByTagName('BODY')[0];
+
+  const mainDiv = document.createElement('div');
+  mainDiv.classList.add('modal');
+  mainDiv.id = 'popup';
+
+  body.appendChild(mainDiv);
+
+  const modalContent = document.createElement('div');
+  modalContent.classList.add('modal-content');
+  mainDiv.appendChild(modalContent);
+
+  const h2 = document.createElement('h2');
+  h2.classList.add('headline');
+  h2.textContent = 'GDPR consent';
+  modalContent.appendChild(h2);
+
+  const legalText = document.createElement('div');
+  legalText.id = 'legal';
+  modalContent.appendChild(legalText);
+
+  const list = document.createElement('div');
+  list.id = 'listOfVendors';
+  modalContent.appendChild(list);
+
+  const btns = document.createElement('div');
+  btns.classList.add('buttons');
+  modalContent.appendChild(btns);
+
+  const acceptButton = document.createElement('button');
+  const rejectButton = document.createElement('button');
+
+  acceptButton.id = 'accept';
+  rejectButton.id = 'reject';
+
+  acceptButton.textContent = 'Accept';
+  rejectButton.textContent = 'Reject';
+
+  btns.appendChild(acceptButton);
+  btns.appendChild(rejectButton);
+
+  expect(Dom.init().mainDiv).toEqual(mainDiv);
+  expect(Dom.init().modalContent).toEqual(modalContent);
+  expect(Dom.init().h2).toEqual(h2);
+  expect(Dom.init().legalText).toEqual(legalText);
+  expect(Dom.init().list).toEqual(list);
+  expect(Dom.init().btns).toEqual(btns);
+  expect(Dom.init().acceptButton).toEqual(acceptButton);
+  expect(Dom.init().rejectButton).toEqual(rejectButton);
+})
+
+  xit("should block scrolling", () => {
+    const body = document.createElement('body');
+    // body.className = 'noscroll';
+    expect(body).toHaveClass('noscroll');
+  })
+
+
+
+})
